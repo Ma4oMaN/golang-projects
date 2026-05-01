f551b551fbd869d942a26805702a90f717936454
ydaureno <daurenov1998@mail.ru>
2026-05-01 14:32:53 +0500

    feat: add StrRev function

---
+++ strrev.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func StrRev(s string) string {
+	runes := []rune(s)
+	i := 0
+	j := len(runes) - 1
+	for i < j {
+		runes[i], runes[j] = runes[j], runes[i]
+		i++
+		j--
+	}
+	return string(runes)
+}
+
+func main() {
+	s := StrRev("Hello, Мир!")
+	fmt.Println(s)
+}
