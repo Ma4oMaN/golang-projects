2cbc43b1473568a55fcec0ffabe6a6193bd0681f
ydaureno <daurenov1998@mail.ru>
2026-05-01 16:46:06 +0500

    feat: add BasicAtoi2 function

---
+++ basicatoi2/basicatoi2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func BasicAtoi2(s string) int {
+	result := 0
+	for _, char := range s {
+		if char < '0' || char > '9' {
+			return 0
+		}
+		digit := int(char - '0')
+		result = result*10 + digit
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(BasicAtoi2("13314fa013"))
+	fmt.Println(BasicAtoi2("4124"))
+	fmt.Println(BasicAtoi2("х481"))
+}
